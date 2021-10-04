77785d61708cbb47419311940e538f2d9931bb9c
Alzhan Nurtaza <alzhan.nurtaza@gmail.com>
2021-10-04 15:42:00 +0600

    manifest added

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ using Qlik.Sense.Client;
 using Newtonsoft.Json;
 using System.Configuration;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
 
 namespace QlikSDKConnect
 {
@@ -35,176 +38,203 @@ namespace QlikSDKConnect
         }
         static void Main(string[] args)
         {
-            //Loading main conf info from app.config
-            Console.OutputEncoding = Encoding.UTF8;
-            string qsUri = ConfigurationManager.AppSettings["QlikSenseUri"];
-            string qsCertPath = ConfigurationManager.AppSettings["QlikSenseCertPath"];
-            string qsDomen = ConfigurationManager.AppSettings["QlikSenseDomen"];
-            string qsUser = ConfigurationManager.AppSettings["QlikSenseUser"];
-            string qsAppId = ConfigurationManager.AppSettings["QlikSenseAppId"];
-            string qsObjectId = ConfigurationManager.AppSettings["QlikSenseObjectId"];
-            string qsBookmarkId = ConfigurationManager.AppSettings["QlikSenseBookmarkId"];
-            string qsFilters = ConfigurationManager.AppSettings["QlikSenseFilters"];
-            string excelSavePath = ConfigurationManager.AppSettings["ExcelSavePath"];
-
-
-            // The default port number is 4747 but can be customized
-            var uri = new Uri(qsUri);
-            var certs = CertificateManager.LoadCertificateFromDirectory(qsCertPath);
-
-            ILocation location = Qlik.Engine.Location.FromUri(uri);
-            location.AsDirectConnection(qsDomen, qsUser, certificateCollection: certs);
-
-
-            AppIdentifier appIdentifier = (AppIdentifier)location.AppWithId(qsAppId);
-            using (var app = location.App(appIdentifier))
+            try
             {
-   
+                //Loading main conf info from app.config
+                Console.OutputEncoding = Encoding.UTF8;
+                string qsUri = ConfigurationManager.AppSettings["QlikSenseUri"];
+                string qsCertPath = ConfigurationManager.AppSettings["QlikSenseCertPath"];
+                string qsDomen = ConfigurationManager.AppSettings["QlikSenseDomen"];
+                string qsUser = ConfigurationManager.AppSettings["QlikSenseUser"];
+                string qsAppId = ConfigurationManager.AppSettings["QlikSenseAppId"];
+                string qsObjectId = ConfigurationManager.AppSettings["QlikSenseObjectId"];
+                string qsBookmarkId = ConfigurationManager.AppSettings["QlikSenseBookmarkId"];
+                string qsFilters = ConfigurationManager.AppSettings["QlikSenseFilters"];
+                string excelSavePath = ConfigurationManager.AppSettings["ExcelSavePath"];
 
-               
-               if(!string.IsNullOrEmpty(qsBookmarkId))
-                    app.ApplyBookmark(qsBookmarkId);
+
+                // The default port number is 4747 but can be customized
+                var uri = new Uri(qsUri);
+                var certs = CertificateManager.LoadCertificateFromDirectory(qsCertPath);
+
+                ILocation location = Qlik.Engine.Location.FromUri(uri);
+                location.AsDirectConnection(qsDomen, qsUser, certificateCollection: certs);
 
 
-                if (!string.IsNullOrEmpty(qsFilters))
+                AppIdentifier appIdentifier = (AppIdentifier)location.AppWithId(qsAppId);
+                using (var app = location.App(appIdentifier))
                 {
-                    FilterBlock filterObject = new FilterBlock();
-                    List<FilterField> fFields = new List<FilterField>();
 
-                    string[] filterBlocks = qsFilters.Split(';').Select(s => s.Trim()).ToArray();
-                    foreach (string block in filterBlocks)
+
+
+                    if (!string.IsNullOrEmpty(qsBookmarkId))
+                        app.ApplyBookmark(qsBookmarkId);
+
+
+                    if (!string.IsNullOrEmpty(qsFilters))
                     {
-                        string fName = block.Split(':')[0];
-                        FilterField fField = new FilterField();
-                        fField.Name = fName;
+                        FilterBlock filterObject = new FilterBlock();
+                        List<FilterField> fFields = new List<FilterField>();
 
-
-
-                        string filterValuePart = block.Split(':')[1];
-                        string[] FilterValues = filterValuePart.Split(',');
-                        List<FilterValue> filterValueList = new List<FilterValue>();
-                        foreach (string value in FilterValues)
+                        string[] filterBlocks = qsFilters.Split(';').Select(s => s.Trim()).ToArray();
+                        foreach (string block in filterBlocks)
                         {
-                            FilterValue fv = new FilterValue();
-                            fv.Value = value;
-                            filterValueList.Add(fv);
-                        }
-                        fField.FilterValues = (filterValueList);
-                        fFields.Add(fField);
-
-                    }
-                    filterObject.Fields = fFields;
-
-                    foreach(FilterField field in filterObject.Fields)
-                    {
-                        var fieldLst = new List<FieldValue>();
-                        
-                        foreach(FilterValue ff in field.FilterValues)
-                        {
-                            FieldValue fieldValue = new FieldValue();
-                            fieldValue.IsNumeric = false;
-                            fieldValue.Text = ff.Value;
-                            fieldLst.Add(fieldValue);
-                        }
-
-                        app.GetField(field.Name).SelectValues(fieldLst, true);
-                    }
-                }
-
-                
+                            string fName = block.Split(':')[0];
+                            FilterField fField = new FilterField();
+                            fField.Name = fName;
 
 
 
-                var qobject = app.GetGenericObject(qsObjectId);
-
-                GenericObjectLayout qlayout = qobject.GetLayout();
-
-
-                dynamic jsonLayout = JsonConvert.DeserializeObject(qlayout.ToString());
-                //Get header from  qDimensionInfo и qMeasureInfo
-                List<ColumnList> columnList = new List<ColumnList>();
-                foreach (var qDim in jsonLayout.qHyperCube.qDimensionInfo)
-                {
-                    ColumnList cl = new ColumnList();
-                    cl.ColumnName = Convert.ToString(qDim.qFallbackTitle);
-                    cl.ColumnType = typeof(string);
-                    columnList.Add(cl);
-                }
-                foreach (var qMes in jsonLayout.qHyperCube.qMeasureInfo)
-                {
-                    ColumnList cl = new ColumnList();
-                    cl.ColumnName = Convert.ToString(qMes.qFallbackTitle);
-                    cl.ColumnType = typeof(double);
-                    columnList.Add(cl);
-                }
-
-
-                //Get column order of HyperCube 
-                List<int> qColumnOrder = jsonLayout.qHyperCube.qColumnOrder.ToObject<List<int>>();
-                List<string> dtColumnList = qColumnOrder.Select(i => columnList[i].ColumnName).ToList();
-                List<Type> dtColumnListType = qColumnOrder.Select(i => columnList[i].ColumnType).ToList();
-
-                //Now creating DataTable and creating columns
-                System.Data.DataTable dt = new System.Data.DataTable();
-                for(int i=0; i<dtColumnList.Count; i++)
-                {
-                    dt.Columns.Add(dtColumnList[i], dtColumnListType[i]);
-                }
-    
-
-                var pager = qobject.GetAllHyperCubePagers().First();
-                var allPages = pager.IteratePages(new[] { new NxPage { Width = pager.NumberOfColumns, Height = 100 } }, Pager.Next).Select(pageSet => pageSet.Single());
-                var datas = allPages.SelectMany(page => page.Matrix);
-                
-                foreach(var data in datas)
-                {
-                    System.Data.DataRow row = dt.NewRow();
-                    int columnIndex = 0;
-                    foreach(var column in dt.Columns)
-                    {
-                        if(data[columnIndex].State.ToString() == "LOCKED")
-                        {
-                            if(data[columnIndex].Num.ToString() != "NaN")
+                            string filterValuePart = block.Split(':')[1];
+                            string[] FilterValues = filterValuePart.Split(',');
+                            List<FilterValue> filterValueList = new List<FilterValue>();
+                            foreach (string value in FilterValues)
                             {
-                                row[columnIndex] = data[columnIndex].Num;
+                                FilterValue fv = new FilterValue();
+                                fv.Value = value;
+                                filterValueList.Add(fv);
+                            }
+                            fField.FilterValues = (filterValueList);
+                            fFields.Add(fField);
+
+                        }
+                        filterObject.Fields = fFields;
+
+                        foreach (FilterField field in filterObject.Fields)
+                        {
+                            var fieldLst = new List<FieldValue>();
+
+                            foreach (FilterValue ff in field.FilterValues)
+                            {
+                                FieldValue fieldValue = new FieldValue();
+                                fieldValue.IsNumeric = false;
+                                fieldValue.Text = ff.Value;
+                                fieldLst.Add(fieldValue);
+                            }
+
+                            app.GetField(field.Name).SelectValues(fieldLst, true);
+                        }
+                    }
+
+
+
+
+
+                    var qobject = app.GetGenericObject(qsObjectId);
+
+                    GenericObjectLayout qlayout = qobject.GetLayout();
+
+
+                    dynamic jsonLayout = JsonConvert.DeserializeObject(qlayout.ToString());
+                    //Get header from  qDimensionInfo и qMeasureInfo
+                    List<ColumnList> columnList = new List<ColumnList>();
+                    foreach (var qDim in jsonLayout.qHyperCube.qDimensionInfo)
+                    {
+                        ColumnList cl = new ColumnList();
+                        cl.ColumnName = Convert.ToString(qDim.qFallbackTitle);
+                        cl.ColumnType = typeof(string);
+                        columnList.Add(cl);
+                    }
+                    foreach (var qMes in jsonLayout.qHyperCube.qMeasureInfo)
+                    {
+                        ColumnList cl = new ColumnList();
+                        cl.ColumnName = Convert.ToString(qMes.qFallbackTitle);
+                        cl.ColumnType = typeof(double);
+                        columnList.Add(cl);
+                    }
+
+
+                    //Get column order of HyperCube 
+                    List<int> qColumnOrder = jsonLayout.qHyperCube.qColumnOrder.ToObject<List<int>>();
+                    List<string> dtColumnList = qColumnOrder.Select(i => columnList[i].ColumnName).ToList();
+                    List<Type> dtColumnListType = qColumnOrder.Select(i => columnList[i].ColumnType).ToList();
+
+                    //Now creating DataTable and creating columns
+                    System.Data.DataTable dt = new System.Data.DataTable();
+                    for (int i = 0; i < dtColumnList.Count; i++)
+                    {
+                        dt.Columns.Add(dtColumnList[i], dtColumnListType[i]);
+                    }
+
+
+                    var pager = qobject.GetAllHyperCubePagers().First();
+                    var allPages = pager.IteratePages(new[] { new NxPage { Width = pager.NumberOfColumns, Height = 100 } }, Pager.Next).Select(pageSet => pageSet.Single());
+                    var datas = allPages.SelectMany(page => page.Matrix);
+
+                    foreach (var data in datas)
+                    {
+                        System.Data.DataRow row = dt.NewRow();
+                        int columnIndex = 0;
+                        foreach (var column in dt.Columns)
+                        {
+                            if (data[columnIndex].State.ToString() == "LOCKED")
+                            {
+                                if (data[columnIndex].Num.ToString() != "NaN")
+                                {
+                                    row[columnIndex] = data[columnIndex].Num;
+                                }
+                                else
+                                {
+                                    row[columnIndex] = DBNull.Value;
+                                }
+
                             }
                             else
                             {
-                                row[columnIndex] = DBNull.Value;
+                                row[columnIndex] = data[columnIndex].Text;
                             }
-                            
+                            columnIndex++;
                         }
-                        else
-                        {
-                            row[columnIndex] = data[columnIndex].Text;
-                        }
-                        columnIndex++;
-                    }
-                    dt.Rows.Add(row);
-                    
-                }
+                        dt.Rows.Add(row);
 
-                ExportToExcel(dt, excelSavePath);
+                    }
+
+                    ExportToExcel(dt, excelSavePath);
+
+                }
+            }
+            catch(Exception ex)
+            {
+                ErrorLogging(ex);
+            }
+            
+
+        }
+        public static void ErrorLogging(Exception ex)
+        {
+            string strPath = @"C:\QlikSDKConnect\Log.txt";
+            if (!File.Exists(strPath))
+            {
+                File.Create(strPath).Dispose();
+            }
+            using (StreamWriter sw = File.AppendText(strPath))
+            {
+                sw.WriteLine("=============Error Logging ===========");
+                sw.WriteLine("===========Start============= " + DateTime.Now);
+                sw.WriteLine("Error Message: " + ex.Message);
+                sw.WriteLine("Stack Trace: " + ex.StackTrace);
+                sw.WriteLine("===========End============= " + DateTime.Now);
 
             }
-
         }
 
         static void ExportToExcel(System.Data.DataTable tbl, string excelFilePath = null)
         {
+            // load excel, and create a new workbook
+            var excelApp = new Excel.Application();
+            excelApp.DisplayAlerts = false;
+            var workBooks = excelApp.Workbooks;
+            var workBook = workBooks.Add();
+            // single worksheet
+            Excel._Worksheet workSheet = workBook.ActiveSheet;
+            workSheet.Name = "output";
+
             try
             {
                 if (tbl == null || tbl.Columns.Count == 0)
                     throw new Exception("ExportToExcel: Null or empty input table!\n");
 
-                // load excel, and create a new workbook
-                var excelApp = new Excel.Application();
-                excelApp.DisplayAlerts = false;
-                excelApp.Workbooks.Add();
 
-                // single worksheet
-                Excel._Worksheet workSheet = excelApp.ActiveSheet;
-                workSheet.Name = "output";
 
                 // column headings
                 for (var i = 0; i < tbl.Columns.Count; i++)
@@ -226,25 +256,33 @@ namespace QlikSDKConnect
                 // check file path
                 if (!string.IsNullOrEmpty(excelFilePath))
                 {
-                    try
-                    {
-                        workSheet.SaveAs(excelFilePath);
-                        excelApp.Quit();
-                    }
-                    catch (Exception ex)
-                    {
-                        throw new Exception("ExportToExcel: Excel file could not be saved! Check filepath.\n"
-                                            + ex.Message);
-                    }
-                }
-                else
-                { // no file path is given
-                    excelApp.Visible = true;
+                    workBook.SaveCopyAs(@excelFilePath);
+                    workBooks.Close();
+                    excelApp.Quit();
+                    if (workSheet != null) Marshal.ReleaseComObject(workSheet);
+                    if (workBooks != null) Marshal.ReleaseComObject(workBooks);
+                    if (workBook != null) Marshal.ReleaseComObject(workBook);
+                    if (excelApp != null) Marshal.ReleaseComObject(excelApp);
+                    workBooks = null;
+                    workBook = null;
+                    excelApp = null;
+                    GC.Collect();
+
                 }
             }
             catch (Exception ex)
             {
-                throw new Exception("ExportToExcel: \n" + ex.Message);
+                ErrorLogging(ex);
+                workBooks.Close();
+                excelApp.Quit();
+                if (workSheet != null) Marshal.ReleaseComObject(workSheet);
+                if (workBooks != null) Marshal.ReleaseComObject(workBooks);
+                if (workBook != null) Marshal.ReleaseComObject(workBook);
+                if (excelApp != null) Marshal.ReleaseComObject(excelApp);
+                workBooks = null;
+                workBook = null;
+                excelApp = null;
+                GC.Collect();
             }
         }
     }
